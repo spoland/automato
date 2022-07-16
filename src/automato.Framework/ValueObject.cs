da681c99c1a055eccab352b50f99b0a7f4ceabb8
Stephen Poland <stephen.poland@outlook.com>
2022-07-16 20:30:49 +0100

    Add equality tests to ActionId

--- src/automato.Framework/ValueObject.cs
+++ src/automato.Framework/ValueObject.cs
@@ -1,7 +1,21 @@
 ﻿namespace automato.Framework;
 
-public abstract class ValueObject
+public abstract class ValueObject : IEqualityComparer<ValueObject>
 {
+    public virtual bool Equals(ValueObject? x, ValueObject? y) => x == y;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
     protected static bool EqualOperator(ValueObject? left, ValueObject? right)
     {
         if (left is null ^ right is null)
@@ -19,19 +33,35 @@ public abstract class ValueObject
 
     protected abstract IEnumerable<object> GetEqualityComponents();
 
-    public override bool Equals(object? obj)
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Blocker Code Smell", "S3875:\"operator==\" should not be overloaded on reference types", Justification = "Derived classes should be treated as value objects.")]
+    public static bool operator ==(ValueObject? a, ValueObject? b)
     {
-        if (obj == null || obj.GetType() != GetType())
+        if (a is null && b is null)
+        {
+            return true;
+        }
+
+        if (a is null || b is null)
         {
             return false;
         }
 
-        var other = (ValueObject)obj;
+        return a.Equals(b);
+    }
 
-        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    public static bool operator !=(ValueObject? a, ValueObject? b)
+    {
+        return !(a == b);
     }
 
     public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+
+    public int GetHashCode(ValueObject obj)
     {
         return GetEqualityComponents()
             .Select(x => x != null ? x.GetHashCode() : 0)
--- test/automato.Domain.Unit.Tests/Actions/ActionIdTests.cs
+++ test/automato.Domain.Unit.Tests/Actions/ActionIdTests.cs
@@ -46,5 +46,19 @@ namespace automato.Domain.Unit.Tests.Actions
             // Assert
             Assert.Throws<ArgumentException>(() => ActionId.FromGuid(guid));
         }
+
+        [Fact]
+        public void Equality_WhenEqual_ShouldReturnTrue()
+        {
+            // Arrange
+            var guid = Guid.NewGuid();
+            var id1 = ActionId.FromGuid(guid);
+            var id2 = ActionId.FromGuid(guid);
+
+            // Assert
+            Assert.Equal(id1, id2);
+            Assert.True(id1.Equals(id2));
+            Assert.True(id1 == id2);
+        }
     }
 }
