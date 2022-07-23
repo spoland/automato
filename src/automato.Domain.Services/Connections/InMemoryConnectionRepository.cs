f4c9f17211e0e69ea656d132d824d874380bfbc8
Stephen Poland <stephen.poland@outlook.com>
2022-07-23 19:35:01 +0100

    Move seed data to in memory repo

--- src/automato.Domain.Services/Connections/InMemoryConnectionRepository.cs
+++ src/automato.Domain.Services/Connections/InMemoryConnectionRepository.cs
@@ -1,10 +1,21 @@
 ﻿using automato.Domain.Connections;
+using automato.Domain.Sftp;
 
 namespace automato.Domain.Services.Connections;
 
 public class InMemoryConnectionRepository : IConnectionRepository
 {
-    private readonly Dictionary<ConnectionId, Connection> _connections = new Dictionary<ConnectionId, Connection>();
+    private readonly Dictionary<ConnectionId, Connection> _connections = new Dictionary<ConnectionId, Connection>()
+    {
+        {
+            ConnectionId.FromGuid(Guid.Parse("0d7b7f38-7345-49c3-8eb8-9855b08ee37d")),
+            new SftpConnection(id: ConnectionId.FromGuid(Guid.Parse("0d7b7f38-7345-49c3-8eb8-9855b08ee37d")), hostname: "1234", username: "spoland", password: "password", name: "Rapidseedbox", portNumber: 2222, details: default, sshHostKeyFingerprint: default)
+        },
+        {
+            ConnectionId.FromGuid(Guid.Parse("0d7b7f38-7345-49c3-8eb8-9855b08ee37e")),
+            new SftpConnection(id: ConnectionId.FromGuid(Guid.Parse("0d7b7f38-7345-49c3-8eb8-9855b08ee37e")), hostname: "1234", username: "spoland", password: "password", name: "Appserver", portNumber: 2222, details: default, sshHostKeyFingerprint: default)
+        }
+    };
 
     public Task Create(Connection connection, CancellationToken cancellationToken)
     {
