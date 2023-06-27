13255a0f4ca95a369b7998f014bdf6ec3f7aad56
Stephen Poland <spoland@eshopworld.com>
2023-06-27 20:31:58 +0100

    Add initial storage

---
+++ src/automato.Infrastructure.Sqlite/AutomatoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace automato.Infrastructure.Sqlite;
+
+public class AutomatoDbContext : DbContext
+{
+    public AutomatoDbContext(DbContextOptions<AutomatoDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ src/automato.Infrastructure.Sqlite/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace automato.Infrastructure.Sqlite;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services)
+    {
+        services.AddDbContext<AutomatoDbContext>(options => options.UseSqlite("Data Source=Data/automato.db;"));
+
+        return services;
+    }
+}
