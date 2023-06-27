e6ecca6050a840d64537aa577633d0eb6546bcbb
Stephen Poland <spoland@eshopworld.com>
2023-06-27 19:47:10 +0100

    Extract local methods for readability

--- src/automato.Infrastructure.SshNet/SshNetSftpService.cs
+++ src/automato.Infrastructure.SshNet/SshNetSftpService.cs
@@ -22,61 +22,78 @@ public class SshNetSftpService : ISftpService
 
         try
         {
-            var connectionInfo = new ConnectionInfo(
-            port: server.Port,
-            host: server.Hostname,
-            username: server.Username,
-            authenticationMethods: new PasswordAuthenticationMethod(username: server.Username, password: server.Password ?? string.Empty));
-
-            using var sftpClient = new SftpClient(connectionInfo);
+            using var sftpClient = new SftpClient(GetConnectionInfo(server));
 
             sftpClient.ErrorOccurred += (sender, args) =>
                 _logger.LogError(args.Exception, "An SFTP error occurred while processing task '{TaskName}': {Message}", task.Name, args.Exception.Message);
 
             sftpClient.Connect();
-            var remoteFiles = await ListDirectoryAsync(sftpClient, task.RemotePath, task.SearchPattern);
 
-            foreach (var file in remoteFiles.Where(rf => !rf.IsDirectory))
-            {
-                cancellationToken.ThrowIfCancellationRequested();
+            var remoteFiles = await ListDirectoryAsync(
+                sftpClient: sftpClient,
+                remotePath: task.RemotePath,
+                searchPattern: task.SearchPattern);
 
-                var localFilePath = Path.Combine(task.LocalPath, file.FullName.Replace(task.RemotePath, ""));
+            await DownloadFiles(
+                task: task,
+                sftpClient: sftpClient,
+                remoteFiles: remoteFiles,
+                cancellationToken: cancellationToken);
 
-                if (!Directory.Exists(Path.GetDirectoryName(localFilePath)))
-                {
-                    _ = Directory.CreateDirectory(Path.GetDirectoryName(localFilePath)!); // use a directory value object
-                }
-
-                using var fileStream = File.Create(localFilePath);
-
-                await sftpClient.DownloadFileAsync(file.FullName, fileStream, (downloaded) => PrintProgress((int)downloaded, file));
-
-                if (task.DeleteDownloadedFiles)
-                {
-                    sftpClient.DeleteFile(file.FullName);
-                    _logger.LogInformation("Deleted downloaded file '{FileName}' from server.", file.FullName);
-                }
-            }
-
-            if (task.DeleteEmptyDirectories)
-            {
-                foreach (var directory in remoteFiles.Where(rf => rf.IsDirectory))
-                {
-                    var filesInDirectory = await ListDirectoryAsync(sftpClient, directory.FullName, task.SearchPattern);
-
-                    if (!filesInDirectory.Any())
-                    {
-                        sftpClient.DeleteDirectory(directory.FullName);
-                        _logger.LogInformation("Deleted empty directory '{Directory}' from server.", directory.FullName);
-                    }
-                }
-            }
+            await DeleteEmptyDirectories(
+                task: task,
+                sftpClient: sftpClient,
+                remoteFiles: remoteFiles);
 
             sftpClient.Disconnect();
+
+            _logger.LogInformation("Successfully completed SFTP download task '{TaskName}'", task.Name);
         }
         catch (Exception e)
         {
             _logger.LogError(e, "An error occurred while processing task '{TaskName}': {Message}", task.Name, e.Message);
+        }
+    }
+
+    private async Task DownloadFiles(SftpDownloadTask task, SftpClient sftpClient, IEnumerable<SftpFile> remoteFiles, CancellationToken cancellationToken)
+    {
+        foreach (var file in remoteFiles.Where(rf => !rf.IsDirectory))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var localFilePath = Path.Combine(task.LocalPath, file.FullName.Replace(task.RemotePath, ""));
+
+            if (!Directory.Exists(Path.GetDirectoryName(localFilePath)))
+            {
+                _ = Directory.CreateDirectory(Path.GetDirectoryName(localFilePath)!);
+            }
+
+            using var fileStream = File.Create(localFilePath);
+
+            await sftpClient.DownloadFileAsync(file.FullName, fileStream, (downloaded) => PrintProgress((int)downloaded, file));
+
+            if (task.DeleteDownloadedFiles)
+            {
+                sftpClient.DeleteFile(file.FullName);
+                _logger.LogInformation("Deleted downloaded file '{FileName}' from server.", file.FullName);
+            }
+        }
+    }
+
+    private async Task DeleteEmptyDirectories(SftpDownloadTask task, SftpClient sftpClient, IEnumerable<SftpFile> remoteFiles)
+    {
+        if (task.DeleteEmptyDirectories)
+        {
+            foreach (var directory in remoteFiles.Where(rf => rf.IsDirectory))
+            {
+                var filesInDirectory = await ListDirectoryAsync(sftpClient, directory.FullName, task.SearchPattern);
+
+                if (!filesInDirectory.Any())
+                {
+                    sftpClient.DeleteDirectory(directory.FullName);
+                    _logger.LogInformation("Deleted empty directory '{Directory}' from server.", directory.FullName);
+                }
+            }
         }
     }
 
@@ -102,5 +119,14 @@ public class SshNetSftpService : ISftpService
     private void PrintProgress(int downloaded, SftpFile sftpFile)
     {
         _logger.LogInformation("Downloading file {FileName}, progress: {Downloaded}%", sftpFile.Name, (downloaded / (double)sftpFile.Attributes.Size * 100).ToString("0.00"));
+    }
+
+    private ConnectionInfo GetConnectionInfo(SftpServer serverConfiguration)
+    {
+        return new ConnectionInfo(
+            port: serverConfiguration.Port,
+            host: serverConfiguration.Hostname,
+            username: serverConfiguration.Username,
+            authenticationMethods: new PasswordAuthenticationMethod(username: serverConfiguration.Username, password: serverConfiguration.Password ?? string.Empty));
     }
 }
