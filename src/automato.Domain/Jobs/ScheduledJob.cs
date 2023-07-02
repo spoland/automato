4b8306633ca2d7795a29b729fe806b3eab746196
Stephen Poland <spoland@eshopworld.com>
2023-07-02 19:37:24 +0100

    Add scheduled job

---
+++ src/automato.Domain/Jobs/ScheduledJob.cs
@@ -0,0 +1,45 @@
+﻿using automato.Domain.Framework;
+
+namespace automato.Domain.Jobs;
+
+public class ScheduledJob : IEntity
+{
+    public static Result<ScheduledJob> Create(string name, Framework.Task task, CronExpression cronExpression)
+    {
+        var validationExceptions = new List<ValidationException>();
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            validationExceptions.Add(new ValidationException(
+                propertyName: nameof(name),
+                message: $"{nameof(name)} must not be null or whitespace."));
+        }
+
+        if (validationExceptions.Any())
+        {
+            return validationExceptions;
+        }
+
+        return new ScheduledJob(
+            name: name,
+            scheduledTaskId: task.Id,
+            id: new Guid().ToString(),
+            cronExpression: cronExpression);
+    }
+
+    public string Id { get; }
+
+    public string Name { get; }
+
+    public string ScheduledTaskId { get; }
+
+    public CronExpression CronExpression { get; }
+
+    private ScheduledJob(string id, string name, string scheduledTaskId, CronExpression cronExpression)
+    {
+        Id = id;
+        Name = name;
+        CronExpression = cronExpression;
+        ScheduledTaskId = scheduledTaskId;
+    }
+}
---
+++ test/automato.Domain.Unit.Tests/Jobs/ShcheduledJobTests.cs
@@ -0,0 +1,67 @@
+﻿using automato.Domain.Jobs;
+
+namespace automato.Domain.Unit.Tests.Jobs;
+
+public class ScheduledJobTests
+{
+    [Fact]
+    public void Create_WhenValid_ShouldCreateInstance()
+    {
+        // Arrange
+        var task = new TestTask(id: "1", name: "Test Task");
+        var name = "Test Job";
+        var cronExpression = CronExpression.Create(
+            minute: "*",
+            hour: "*",
+            dayOfMonth: "*",
+            month: "*",
+            dayOfWeek: "*").Value!;
+
+        // Act
+        var result = ScheduledJob.Create(
+            name: name,
+            task: task,
+            cronExpression: cronExpression);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value?.Name.Should().Be(name);
+        result.Value?.Id.Should().NotBeNullOrEmpty();
+        result.Value?.ScheduledTaskId.Should().Be(task.Id);
+        result.Value?.CronExpression.Should().Be(cronExpression);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void Create_WhenInvalid_ShouldCreateInstance(string name)
+    {
+        // Arrange
+        var task = new TestTask(id: "1", name: "Test Task");
+        var cronExpression = CronExpression.Create(
+            minute: "*",
+            hour: "*",
+            dayOfMonth: "*",
+            month: "*",
+            dayOfWeek: "*").Value!;
+
+        // Act
+        var result = ScheduledJob.Create(
+            name: name,
+            task: task,
+            cronExpression: cronExpression);
+
+        // Assert
+        result.Value.Should().BeNull();
+        result.IsSuccess.Should().BeFalse();
+        result.Exceptions.Should().HaveCount(1);
+    }
+
+    private class TestTask : Framework.Task
+    {
+        public TestTask(string id, string name) : base(id, name)
+        {
+        }
+    }
+}
