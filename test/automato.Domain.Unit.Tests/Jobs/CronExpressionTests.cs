4751db59dd262e8672a48472b5475c34e6a36a88
Stephen Poland <spoland@eshopworld.com>
2023-07-02 19:25:13 +0100

    Fix tests

--- test/automato.Domain.Unit.Tests/Jobs/CronExpressionTests.cs
+++ test/automato.Domain.Unit.Tests/Jobs/CronExpressionTests.cs
@@ -60,22 +60,47 @@ public class CronExpressionTests
     public void TryParse_WhenInvalid_ShouldReturnFalse(string stringExpression)
     {
         // Act
-        var success = CronExpression.TryParse(stringExpression, out var result);
+        var success = CronExpression.TryParse(stringExpression, default, out var result);
 
         // Assert
         success.Should().BeFalse();
         result.Should().BeNull();
     }
 
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    [InlineData("1 1 1 1")]
+    [InlineData("-1 -1 0 0 -1")]
+    [InlineData("60 24 32 13 7")]
+    public void Parse_WhenInvalid_ShouldThrow(string stringExpression)
+    {
+        // Act
+        Action parse = () => CronExpression.Parse(stringExpression, default);
+
+        // Assert
+        parse.Should().Throw<Exception>();
+    }
+
     [Fact]
     public void TryParse_WhenValid_ShouldReturnTrue()
     {
         // Act
-        var success = CronExpression.TryParse("* * * 1 *", out var result);
+        var success = CronExpression.TryParse("* * * 1 *", default, out var result);
 
         // Assert
         success.Should().BeTrue();
         result?.Value.Should().Be("* * * 1 *");
     }
 
+    [Fact]
+    public void Parse_WhenValid_ShouldReturnCronExpression()
+    {
+        // Act
+        var cronExpression = CronExpression.Parse("* * * 1 *", default);
+
+        // Assert
+        cronExpression.ToString().Should().Be("* * * 1 *");
+    }
 }
